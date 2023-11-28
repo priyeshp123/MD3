31b0f806d4134fea242ee0611b3ea20fd15af162
priyeshp123 <priyesh3386@yahoo.co.uk>
2023-11-28 08:16:33 +0000

    Update Jupyter Notebooks

--- notebook/Yahoo/yahoo_data_ext_kit.py
+++ notebook/Yahoo/yahoo_data_ext_kit.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
 
 def extract_statements(stype,master_company_data):
     
@@ -47,5 +51,99 @@ def remove_non_annual_data(df):
     df_company.drop(df_company[df_company['M'] != mode_M].index, axis=0, inplace=True)
     
     df_new = pd.concat([df_new,df_company],axis=0)
+    
+def chk_trend(S):
+  
+  S = pd.DataFrame(S)
+
+  trend_details={}
+
+  scaler = preprocessing.MinMaxScaler()
+  scaler = scaler.fit_transform(S)* 10
+  x=[1,2,3,4]
+
+  y=[]
+
+  for i in range(0,len(scaler)):
+    y = y + list(scaler[i])
+
+  coeff=np.polyfit(x,y,deg=1)
+  f = np.poly1d(coeff)
+
+    
+  trend_details['gradient'] = coeff[0]
+  trend_details['x_val'] = x
+  trend_details['y_val'] = y
+    
+  return trend_details
 
   return df_new
+
+def getTrendScore(S):
+
+   trend_details = chk_trend(S)
+
+   return round(trend_details['gradient'])
+
+def show_trend(trend_details,deg=1):
+ 
+  x = trend_details['x_val']
+  y = trend_details['y_val']
+  print(x)
+  print(y)
+
+  xx=np.linspace(0,5,100)
+
+  # Linear
+
+  deg = 1
+
+  coeff=np.polyfit(x,y,deg)
+  f = np.poly1d(coeff)
+  y1 = f(x)
+  y1_line=f(xx)
+
+  y1_error = mean_squared_error(y,y1)   
+  print("y1_error: %.2f" % y1_error)
+
+  # Polynominal 1
+
+  deg = 2
+  coeff=np.polyfit(x,y,deg)
+  f = np.poly1d(coeff)
+  y2 = f(x)
+  y2_line=f(xx)
+
+  y2_error = mean_squared_error(y,y2)   
+  print("y2_error: %.2f" % y2_error)
+    
+  # Polynomial 2
+
+  deg = 3
+    
+  coeff=np.polyfit(x,y,deg)
+  f = np.poly1d(coeff)
+  y3 = f(x)
+  y3_line=f(xx)
+
+  # Polynomial 2
+
+  deg = 3
+    
+  coeff=np.polyfit(x,y,deg)
+  f = np.poly1d(coeff)
+  y3 = f(x)
+  y3_line=f(xx)
+
+  y3_error = mean_squared_error(y,y3)   
+  print("y3_error: %.2f" % y3_error)
+  print(" ")
+
+  plt.plot(xx,y1_line, c='red')
+  plt.plot(xx,y2_line, c='green')
+  plt.plot(xx,y3_line, c='red')
+    
+  plt.scatter(x,y,c="black")
+
+  plt.show()
+
